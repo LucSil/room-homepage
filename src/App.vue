599415e798a2fe3c37f8516b98a461a89231ad96
LucSil <luciansilva351@gmail.com>
2022-05-08 16:42:22 +0100

    imported Hero.vue into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
-<template></template>
+<template>
+  <div>
+    <Hero />
+  </div>
+</template>
 
 <script>
+import Hero from "./components/Hero.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Hero,
+  },
 };
 </script>
 
