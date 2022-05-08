3fc44deb307479ea9d8a692eddfbd17f8d722bfc
LucSil <luciansilva351@gmail.com>
2022-05-08 18:01:57 +0100

    fonts from googlefonts.com

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        spartan: ["League Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
