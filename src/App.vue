766aed1daa01d7a92613f90be05a7439829a9209
LucSil <luciansilva351@gmail.com>
2022-05-12 16:34:03 +0100

    imported Images.vue into App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <Hero />
     <Main />
   </section>
+  <Images />
 </template>
 
 <script>
 import Hero from "./components/firstGrid/Hero.vue";
 import Main from "./components/firstGrid/Main.vue";
+import Images from "./components/secondGrid/Images.vue";
 
 export default {
   name: "App",
   components: {
     Hero,
     Main,
+    Images,
   },
 };
 </script>
