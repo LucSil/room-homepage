b351f998efe3028caef77e0b636e1ba028c6d599
LucSil <luciansilva351@gmail.com>
2022-05-12 17:41:05 +0100

    styling and templating for About.vue complete

--- src/components/secondGrid/About.vue
+++ src/components/secondGrid/About.vue
@@ -1,8 +1,10 @@
 <template>
-  <section class="py-14 px-5">
-    <h2>ABOUT OUR FURNITURE</h2>
-    <p>
-      Outr multifunctional collection blends design and function to suit your
+  <section class="py-14">
+    <h2 class="ml-10 mb-3 font-semibold text-base tracking-[0.25rem]">
+      ABOUT OUR FURNITURE
+    </h2>
+    <p class="px-10 text-sm text-gray-400">
+      Our multifunctional collection blends design and function to suit your
       individual taste. Make each room unique, or pick a cohesive theme that
       best express your interests and what inspires yourFind tthe furniture
       pieces you need, from traditional to contemporary styles or anything in
