61870a6af6a7326ef7fc63896de2c996ae147701
LucSil <luciansilva351@gmail.com>
2022-05-12 15:39:37 +0100

    imported About.vue into Images.vue; templating for Images.vue complete

--- src/components/secondGrid/Images.vue
+++ src/components/secondGrid/Images.vue
@@ -1,4 +1,35 @@
-<template></template>
+<template>
+  <section>
+    <!-- 1st -->
+    <picture>
+      <source
+        media="(min-width:600px)"
+        srcset="../../assets/images/bg/black-d.jpg"
+      />
+      <source
+        media="(min-width:310px)"
+        srcset="../../assets/images/bg/black-m.jpg"
+      />
+      <img src="../../assets/images/bg/black-d.jpg" alt="#" />
+    </picture>
+
+    <!-- 2nd -->
+    <About />
+
+    <!-- 3rd -->
+    <picture>
+      <source
+        media="(min-width:600px)"
+        srcset="../../assets/images/bg/white-d.jpg"
+      />
+      <source
+        media="(min-width:310px)"
+        srcset="../../assets/images/bg/white-m.jpg"
+      />
+      <img src="../../assets/images/bg/white-d.jpg" alt="#" />
+    </picture>
+  </section>
+</template>
 
 <script>
 export default {
