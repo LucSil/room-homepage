68f1f15b2ff0c453dcc113d701cecb5336d0eff7
LucSil <luciansilva351@gmail.com>
2022-05-12 17:02:46 +0100

    additional styling for Main.vue

--- src/components/firstGrid/Main.vue
+++ src/components/firstGrid/Main.vue
@@ -1,9 +1,11 @@
 <template>
-  <section class="px-5 py-16">
+  <section class="px-10 py-20">
     <!-- Div containing title and further information -->
     <div>
-      <h1 class="text-2xl font-bold">Discover innovative ways to decorate</h1>
-      <p>
+      <h1 class="text-4xl leading-8 font-medium mb-4">
+        Discover innovative ways to decorate
+      </h1>
+      <p class="text-gray-400 text-sm">
         We provide unmatched quality, comfort, and style for property owners
         across the country. Our experts combine form and function in bringing
         your vision to life. Create a room in your own style with our collection
