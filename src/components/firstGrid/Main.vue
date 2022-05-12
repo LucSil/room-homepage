75dbeb791c8016a544158a8196227ffcc0f972e6
LucSil <luciansilva351@gmail.com>
2022-05-12 09:51:30 +0100

    initial templating for Main.vue done

---
+++ src/components/firstGrid/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="px-5 py-16">
+    <!-- Div containing title and further information -->
+    <div>
+      <h1 class="text-2xl font-bold">Discover innovative ways to decorate</h1>
+      <p>
+        We provide unmatched quality, comfort, and style for property owners
+        across the country. Our experts combine form and function in bringing
+        your vision to life. Create a room in your own style with our collection
+        and make your property a reflection of you and what you love.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
