7245f1c49b7890f8c58a2c31bd779959376efb10
LucSil <luciansilva351@gmail.com>
2022-05-12 16:49:57 +0100

    initial templating for About.vue complete

--- src/components/secondGrid/About.vue
+++ src/components/secondGrid/About.vue
@@ -1,4 +1,16 @@
-<template></template>
+<template>
+  <section class="py-14 px-5">
+    <h2>ABOUT OUR FURNITURE</h2>
+    <p>
+      Outr multifunctional collection blends design and function to suit your
+      individual taste. Make each room unique, or pick a cohesive theme that
+      best express your interests and what inspires yourFind tthe furniture
+      pieces you need, from traditional to contemporary styles or anything in
+      between. Product specialists are available to help you create your dream
+      space.
+    </p>
+  </section>
+</template>
 
 <script>
 export default {
