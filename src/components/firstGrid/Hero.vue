386161ce8f512c5f4e28b3a84cd36144b3663b95
LucSil <luciansilva351@gmail.com>
2022-05-13 08:14:26 +0100

    moved navbar into Navbar.vue

--- src/components/firstGrid/Hero.vue
+++ src/components/firstGrid/Hero.vue
@@ -22,45 +22,6 @@
         <ion-icon id="next" name="chevron-forward-sharp"></ion-icon>
       </button>
     </div>
-
-    <!-- Navbar and Menu -->
-    <div id="hero-items" class="absolute z-10 top-0 w-full">
-      <nav class="mt-12">
-        <!-- menu open and logo -->
-        <div id="mobile-menu" class="flex items-center justify-center">
-          <button @click="openMenu" id="hamburger" class="absolute left-0 ml-5">
-            <ion-icon name="menu-sharp"></ion-icon>
-          </button>
-          <div>
-            <img src="../../assets/images/svg/logo.svg" alt="menu button" />
-          </div>
-        </div>
-      </nav>
-
-      <!-- Modal Background -->
-      <transition name="modal">
-        <div v-if="menu" class="fixed inset-0 bg-black opacity-60 z-20"></div>
-      </transition>
-
-      <!-- menu close and nav-links -->
-      <transition name="menu">
-        <div
-          v-if="menu"
-          id="nav-menu-items"
-          class="bg-white absolute z-30 top-0 w-full px-5 flex justify-between items-center py-10"
-        >
-          <button @click="closeMenu" id="close-menu" class="mr-14">
-            <ion-icon name="close"></ion-icon>
-          </button>
-          <ul id="nav-links" class="flex justify-between items-center w-full">
-            <li><a href="#">home</a></li>
-            <li><a href="#">shop</a></li>
-            <li><a href="#">about</a></li>
-            <li><a href="#">contact</a></li>
-          </ul>
-        </div>
-      </transition>
-    </div>
   </section>
 </template>
 
