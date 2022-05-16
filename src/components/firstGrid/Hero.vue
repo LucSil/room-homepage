1e2726434d1413b04f8bfb773c959b68952dc521
LucSil <luciansilva351@gmail.com>
2022-05-16 09:03:02 +0100

    implemented carousel with vue-agile

--- src/components/firstGrid/Hero.vue
+++ src/components/firstGrid/Hero.vue
@@ -1,24 +1,53 @@
 <template>
   <section id="hero" class="relative">
     <!-- Background Image -->
-    <picture>
-      <source
-        media="(min-width:600px)"
-        srcset="../../assets/images/bg/white-d.jpg"
-      />
-      <source
-        media="(min-width:310px)"
-        srcset="../../assets/images/bg/white-m.jpg"
-      />
-      <img src="../../assets/images/bg/white-d.jpg" alt="#" />
-    </picture>
+    <agile :navButtons="false" ref="carousel">
+      <!-- White Bg -->
+      <picture id="one">
+        <source
+          media="(min-width:600px)"
+          srcset="../../assets/images/bg/white-m.jpg"
+        />
+        <source
+          media="(min-width:310px)"
+          srcset="../../assets/images/bg/white-m.jpg"
+        />
+        <img src="../../assets/images/bg/white-d.jpg" alt="#" />
+      </picture>
+
+      <!-- Black Bg -->
+      <picture id="two">
+        <source
+          media="(min-width:600px)"
+          srcset="../../assets/images/bg/black-d.jpg"
+        />
+        <source
+          media="(min-width:310px)"
+          srcset="../../assets/images/bg/black-m.jpg"
+        />
+        <img src="../../assets/images/bg/black-d.jpg" alt="#" />
+      </picture>
+
+      <!-- Yellow Bg -->
+      <picture id="three">
+        <source
+          media="(min-width:600px)"
+          srcset="../../assets/images/bg/yellow-d.jpg"
+        />
+        <source
+          media="(min-width:310px)"
+          srcset="../../assets/images/bg/yellow-m.jpg"
+        />
+        <img src="../../assets/images/bg/yellow-d.jpg" alt="#" />
+      </picture>
+    </agile>
 
     <!-- Image Switch Buttons -->
     <div id="switch">
-      <button>
+      <button @click="$refs.carousel.goToPrev()">
         <ion-icon id="previous" name="chevron-back-sharp"></ion-icon>
       </button>
-      <button>
+      <button @click="$refs.carousel.goToNext()">
         <ion-icon id="next" name="chevron-forward-sharp"></ion-icon>
       </button>
     </div>
@@ -30,8 +59,18 @@
 
 <script>
 import Navbar from "../Navbar.vue";
+import { VueAgile } from "vue-agile";
 export default {
   name: "Hero",
-  components: { Navbar },
+  components: { Navbar, agile: VueAgile },
+  data() {
+    return {
+      val: 1,
+    };
+  },
+  methods: {
+    prev() {},
+    next() {},
+  },
 };
 </script>
