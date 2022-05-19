119da9f1637d1abe7cdc304ecdc5e6b1c05d0375
LucSil <luciansilva351@gmail.com>
2022-05-19 17:17:28 +0100

    deleted extra text div

--- src/components/firstGrid/Main.vue
+++ src/components/firstGrid/Main.vue
@@ -2,42 +2,17 @@
   <section class="px-10 py-16 lg:py-0 lg:px-[4.7rem]">
     <!-- Div containing title and further information -->
     <div class="relative lg:top-[25%]">
-      <agile :navButtons="false" ref="text">
-        <!-- Slide 1 -->
-        <div class="mainText">
-          <h1>Discover innovative ways to decorate</h1>
-          <p>
-            We provide unmatched quality, comfort, and style for property owners
-            across the country. Our experts combine form and function in
-            bringing your vision to life. Create a room in your own style with
-            our collection and make your property a reflection of you and what
-            you love.
-          </p>
-        </div>
-
-        <!-- Slide 2 -->
-        <div class="mainText">
-          <h1>We are available all across the globe</h1>
-          <p>
-            With stores all over the world, it's easy for you to find furniture
-            for your home or place of business. Locally, we're in most major
-            cities throughout the country. Find the branch nearest you using our
-            store locator. Any questions? Don't hesitate to contact us today.
-          </p>
-        </div>
-
-        <!-- Slide 3 -->
-        <div class="mainText">
-          <h1>Manufactured with the best materials</h1>
-          <p>
-            Our modern furniture store provide a high level of quality. Our
-            company has invested in advanced technology to ensure that every
-            product is made as perfect and as consistent as possible. With three
-            decades of experience in this industry, we understand what customers
-            want for their home and office.
-          </p>
-        </div>
-      </agile>
+      <!-- Slide 1 -->
+      <div class="mainText">
+        <h1>Discover innovative ways to decorate</h1>
+        <p>
+          We provide unmatched quality, comfort, and style for property owners
+          across the country. Our experts combine form and function in bringing
+          your vision to life. Create a room in your own style with our
+          collection and make your property a reflection of you and what you
+          love.
+        </p>
+      </div>
 
       <a href="#" class="flex items-center group lg:w-[60%]">
         <span class="group-hover:text-gray-400 text-base tracking-[0.5rem]"
